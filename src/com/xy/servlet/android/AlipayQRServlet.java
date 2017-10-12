7151d7195749980464f4586cb21663cbc3ea8e84
jat <919700667@qq.com>
2017-10-12 22:58:21 +0800

    order show message

--- src/com/xy/servlet/android/AlipayQRServlet.java
+++ src/com/xy/servlet/android/AlipayQRServlet.java
@@ -149,7 +149,7 @@ public class AlipayQRServlet extends HttpServlet {
 		String outTradeNo = out_trade_no;
 
 		// (必填) 订单标题，粗略描述用户的支付目的。如“xxx品牌xxx门店当面付扫码消费”
-		String subject = subject_str;
+		String subject = body_str;
 
 		// (必填) 订单总金额，单位为元，不能超过1亿元
 		// 如果同时传入了【打折金额】,【不可打折金额】,【订单总金额】三者,则必须满足如下条件:【订单总金额】=【打折金额】+【不可打折金额】
